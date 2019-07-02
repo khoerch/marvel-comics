483ae85da48aa71c327449ce0425f3fa83a234f6
Kevin Hoercher <khoerch@gmail.com>
2019-07-02 10:55:01 -0400

    Complete video proof of concept

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 
 //Should I create an object with API endpoints and keys as key value pairs?
 const apiKeyYoutube = 'AIzaSyCjJlcZ8mjV7x4ZMHjBJ-FisBk_K2hZeG0';
+const apiKeyMarvel = 'e7fbac215917563a2dd01a2a025fdada';
+const apiKeyComicVine = 'db461332518168bcf3a79484a73ad6fb11f7dc5b';
 
-function displayOrigin() {
+function displayOrigin(originJson) {
     //Display the origin for the searched character from Comic Vine's API
+    console.log(originJson);
 }
 
 function displayVideos(videoJson) {
     //Display videos from YouTube based on the searched character
     console.log(videoJson);
     $('#video-results').empty();
-    $('#video-results').html(`
-        <p>hello!</p>
-    `);
+    for (let i = 0; i < videoJson.items.length; i++) {
+        $('#video-results').html(`
+        <li>
+            <p>${videoJson.items[i].snippet.title}</p>
+            <img src='${videoJson.items[i].snippet.thumbnails.default.url}'>
+        </li>
+        `);
+    }
 }
 
 function displayComics() {
@@ -30,8 +38,23 @@ function createStrings() {
 
 function getRequests(searchTerm) {
     //Based on the search term, call the separate API requests and pass JSON messages to next functions to display the results
+    const urlOrigin = `https://comicvine.gamespot.com/api/search/?api_key=${apiKeyComicVine}&format=json&resources=volume&field_list=name&query=${searchTerm}`;
+
     const urlVideo = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${encodeURIComponent(`history of ${searchTerm}`)}&type=video&key=${apiKeyYoutube}`; 
-    console.log(urlVideo);
+    
+    console.log(urlOrigin);
+
+    fetch(urlOrigin)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayOrigin(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`)
+        });
 
     fetch(urlVideo)
         .then(response => {
