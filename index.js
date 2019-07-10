e247f0ed5324d545e3ed8e0791cdc4c6b7665394
Kevin Hoercher <khoerch@gmail.com>
2019-07-10 15:56:26 -0400

    Fix an insecure link

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function displayComics(comicJson) {
       $('#js-comics').append(`
       <li>
         <p>${comicJson[i].title}</p>
-        <a href="${comicJson[i].urls[0].url}" target="_blank"><img src="${comicJson[i].images[0].path}.${comicJson[i].images[0].extension}" alt="Issue cover"/></a>
+        <a href="${comicJson[i].urls[0].url}" target="_blank"><img src="https${comicJson[i].images[0].path.slice(4)}.${comicJson[i].images[0].extension}" alt="Issue cover"/></a>
         <a href="${comicJson[i].urls[0].url}" target="_blank">Start Reading!</a>
       </li>
     `)
